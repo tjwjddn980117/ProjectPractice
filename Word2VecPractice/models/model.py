a45505177effa096b11efc4737cc08717e906834
tjwjddn980117 <tjwjddn15584@naver.com>
2023-12-05 09:00:48 +0900

    .

--- Word2VecPractice/models/model.py
+++ Word2VecPractice/models/model.py
@@ -24,3 +24,25 @@ class CBOW_Model(nn.Module):
         x = x.mean(axis=1)
         x = self.linear(x)
         return x
+
+class SkipGram_Model(nn.Module):
+    """
+    Implementation of Skip-Gram model described in paper:
+    https://arxiv.org/abs/1301.3781
+    """
+    def __init__(self, vocab_size: int):
+        super(SkipGram_Model, self).__init__()
+        self.embeddings = nn.Embedding(
+            num_embeddings=vocab_size,
+            embedding_dim=EMBED_DIMENSION,
+            max_norm=EMBED_MAX_NORM,
+        )
+        self.linear = nn.Linear(
+            in_features=EMBED_DIMENSION,
+            out_features=vocab_size,
+        )
+
+    def forward(self, inputs_):
+        x = self.embeddings(inputs_)
+        x = self.linear(x)
+        return x
