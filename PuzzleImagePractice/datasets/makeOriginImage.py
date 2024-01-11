21923c2aafecd1228c56e4a156ebb1a50770382e
tjwjddn980117 <tjwjddn15584@naver.com>
2024-01-11 09:03:34 +0900

    .

---
+++ PuzzleImagePractice/datasets/makeOriginImage.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+from torchvision import transforms
+import torchvision.models as models
+
+import random
+from tqdm.auto import tqdm
+import os
+mydir = os.getcwd()
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import warnings
+
+def seed_everything(seed):
+    '''
+    define seed with fixed value.
+    '''
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+seed=42
+seed_everything(seed) # Seed 고정
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+print(device)
+
+data_path = mydir + '\content'
+train_df = pd.read_csv(data_path+'\\train.csv')
+test_df = pd.read_csv(data_path+'\\test.csv')
+
+def check_img_save_origin(train_df, show_num, save_origin=False):
+
+    # 재정렬한 이미지 데이터 프레임 생성
+    dict_origin = {'ID':[], 'img_path':[]}
+
+    for i in range(1,16+1):
+        dict_origin[str(i)] = [i]*len(train_df)
+
+    #  dict_origin ->
+    #  {'ID': [],
+    #   'img_path': [],
+    #   '1': [1, 1, 1, ..., 1], '1'이 len(train_df)만큼 반복
+    #   '2': [2, 2, 2, ..., 2], '2'가 len(train_df)만큼 반복
+    #   ...
+    #   '16': [16, 16, 16, ..., 16] '16'이 len(train_df)만큼 반복}
+
+    # 출력할 이미지 개수 새기
+    count = 0
+
+    # 미리 갯수를 맞춰놓기.
+    # save origin 이면 train_df만큼, 
+    # save origin 이 아니면, 정해놓은 show_num만큼.
+    if save_origin == False:
+        repeat = [i for i in range(show_num)]
+    else:
+        repeat = [i for i in range(len(train_df))]
+
+    for index in tqdm(repeat):
+        sample_df = train_df.iloc[index]
+
+        # train 이미지 불러오기
+        train_path = sample_df['img_path'].split('/')[-1]
+        train_img = Image.open(data_path+'/train/'+train_path)
+        # train_img is the path
+        # ex) ..data/train/TRAIN_00000.jpg
+        raw_img = Image.open(data_path+'/train/'+train_path)    
+
+        # train 이미지에 숫자 표기
+        draw = ImageDraw.Draw(train_img)    
+        width, height = train_img.size  
+        cell_width = width // 4
+        cell_height = height // 4   
+
+        font_size = 50
+        font = ImageFont.truetype("LiberationSans-Regular.ttf", font_size)  
+        # 데이터에 저장되어있는 숫자들. 
+        # ex) 8,1,16,12,5,10,...
+        numbers = list(sample_df)[2:]   
+        for i, number in enumerate(numbers):
+            row = i // 4
+            col = i % 4
+            x = col * cell_width + (cell_width - font_size) // 2
+            y = row * cell_height + (cell_height - font_size) // 2
+            draw.text((x, y), str(number), fill="red", font=font)
+
+
+        i = 0
+        dict_tile = {}
+
+        for row in range(4):
+            for col in range(4):
+                left = col * cell_width
+                upper = row * cell_height
+                right = left + cell_width
+                lower = upper + cell_height
+
+                # 부분 이미지 추출
+                tile = raw_img.crop((left, upper, right, lower))
+                dict_tile[numbers[i]] = tile
+
+                i += 1
