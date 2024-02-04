485d8700c9c30e9505c0a6f9ed3b230f957fc63f
tjwjddn980117 <tjwjddn15584@naver.com>
2024-02-04 10:28:04 +0900

    .

--- conceptOfCrossEntropyLoss_2.py
+++ conceptOfCrossEntropyLoss_2.py
@@ -2,15 +2,24 @@ import torch
 import torch.nn as nn
 
 output = torch.Tensor(
+    [[
+        [0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0]
+    ],
     [
-        [0, 1, 0, 0, 0]
-    ]
+        [0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0]
+    ]]
 )
 
-target = torch.LongTensor([1])
+target = torch.LongTensor([[1,2,3],[1,2,3]])
 
+output = output.transpose(1,2).contiguous()
 criterion = nn.CrossEntropyLoss()
-
+print(output.shape)
+print(target.shape)
 loss = criterion(output, target)
 
 print(loss)
