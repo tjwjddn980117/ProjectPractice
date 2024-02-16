639bc5d6e644f1213f7762d2a58f20ec88260988
tjwjddn980117 <tjwjddn15584@naver.com>
2024-02-16 09:03:34 +0900

    .

--- PuzzleImagePractice/requirements.bat
+++ PuzzleImagePractice/requirements.bat
@@ -11,7 +11,7 @@ call conda create -y -n %ENV_NAME% python=3.9
 echo enter the virtual environment.
 call conda activate %ENV_NAME%
 
-echo start downloading environment for Puzzle.
+echo start downloading environment for %ENV_NAME%.
 call conda install -y pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 call conda install -y conda-forge::matplotlib conda-forge::seaborn conda-forge::tqdm anaconda::pandas
 
