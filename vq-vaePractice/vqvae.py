1ebb88ddb4bbbf25b232de5c1af5fba4e0df4187
tjwjddn980117 <tjwjddn15584@naver.com>
2024-02-23 09:03:48 +0900

    .

---
+++ vq-vaePractice/vqvae.py
@@ -0,0 +1,53 @@
+from torch import nn
+from blocks import Encoder, Decoder
+from blocks_methods import VectorQuantizer
+
+class VQVAE(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        num_hiddens,
+        num_downsampling_layers,
+        num_residual_layers,
+        num_residual_hiddens,
+        embedding_dim,
+        num_embeddings,
+        use_ema,
+        decay,
+        epsilon,
+    ):
+        super().__init__()
+        self.encoder = Encoder(
+            in_channels,
+            num_hiddens,
+            num_downsampling_layers,
+            num_residual_layers,
+            num_residual_hiddens,
+        )
+        self.pre_vq_conv = nn.Conv2d(
+            in_channels=num_hiddens, out_channels=embedding_dim, kernel_size=1
+        )
+        self.vq = VectorQuantizer(
+            embedding_dim, num_embeddings, use_ema, decay, epsilon
+        )
+        self.decoder = Decoder(
+            embedding_dim,
+            num_hiddens,
+            num_downsampling_layers,
+            num_residual_layers,
+            num_residual_hiddens,
+        )
+
+    def quantize(self, x):
+        z = self.pre_vq_conv(self.encoder(x))
+        (z_quantized, dictionary_loss, commitment_loss, encoding_indices) = self.vq(z)
+        return (z_quantized, dictionary_loss, commitment_loss, encoding_indices)
+
+    def forward(self, x):
+        (z_quantized, dictionary_loss, commitment_loss, _) = self.quantize(x)
+        x_recon = self.decoder(z_quantized)
+        return {
+            "dictionary_loss": dictionary_loss,
+            "commitment_loss": commitment_loss,
+            "x_recon": x_recon,
+        }
