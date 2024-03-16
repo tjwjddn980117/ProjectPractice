bdfc5bfa55a169b2b4044a36c76d920372afbb6d
tjwjddn980117 <tjwjddn15584@naver.com>
2024-03-16 23:52:36 +0900

    .

--- diffusioinPractice/attend.py
+++
@@ -1,9 +0,0 @@
-from functools import wraps
-from packaging import version
-from collections import namedtuple
-
-import torch
-from torch import nn, einsum
-import torch.nn.functional as F
-
-from einops import rearrange
---
+++ diffusionPractice/requirements.bat
@@ -0,0 +1,6 @@
+conda install conda-forge::packaging
+conda install conda-forge::einops
+conda install conda-forge::tqdm
+pip install ema-pytorch
+conda install conda-forge::accelerate
+pip install pytorch-fid
