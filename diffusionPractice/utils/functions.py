6de2faef7bd755d04e05d78309b29080364354aa
tjwjddn980117 <tjwjddn15584@naver.com>
2024-03-17 09:09:46 +0900

    .

--- diffusionPractice/utils/functions.py
+++ diffusionPractice/utils/functions.py
@@ -16,3 +16,19 @@ def divisible_by(numer, denom):
 
 def identity(t, *args, **kwargs):
     return t
+
+def cycle(dl):
+    while True:
+        for data in dl:
+            yield data
+
+def has_int_squareroot(num):
+    return (math.sqrt(num) ** 2) == num
+
+def num_to_groups(num, divisor):
+    groups = num // divisor
+    remainder = num % divisor
+    arr = [divisor] * groups
+    if remainder > 0:
+        arr.append(remainder)
+    return arr
