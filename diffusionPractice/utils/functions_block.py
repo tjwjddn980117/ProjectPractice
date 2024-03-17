cbc4e36374a5a465053849afae7f08cb0fc8531a
tjwjddn980117 <tjwjddn15584@naver.com>
2024-03-17 09:14:32 +0900

    .

---
+++ diffusionPractice/utils/functions_block.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+from einops import rearrange, reduce, repeat
+from einops.layers.torch import Rearrange
+from functions import *
+
+# small helper modules
+def Upsample(dim, dim_out = None):
+    return nn.Sequential(
+        nn.Upsample(scale_factor = 2, mode = 'nearest'),
+        nn.Conv2d(dim, default(dim_out, dim), 3, padding = 1)
+    )
+
+def Downsample(dim, dim_out = None):
+    return nn.Sequential(
+        Rearrange('b c (h p1) (w p2) -> b (c p1 p2) h w', p1 = 2, p2 = 2),
+        nn.Conv2d(dim * 4, default(dim_out, dim), 1)
+    )
