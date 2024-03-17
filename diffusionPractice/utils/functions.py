d633e7f817c50c0a60e2b80a535914d087dda81d
tjwjddn980117 <tjwjddn15584@naver.com>
2024-03-17 09:09:17 +0900

    .

---
+++ diffusionPractice/utils/functions.py
@@ -0,0 +1,18 @@
+def exists(x):
+    return x is not None
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if callable(d) else d
+
+def cast_tuple(t, length = 1):
+    if isinstance(t, tuple):
+        return t
+    return ((t,) * length)
+
+def divisible_by(numer, denom):
+    return (numer % denom) == 0
+
+def identity(t, *args, **kwargs):
+    return t
