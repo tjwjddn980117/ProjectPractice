6e77d114e8b5a233409e15e58f2e7205705ec8ca
tjwjddn980117 <tjwjddn15584@naver.com>
2024-03-19 08:43:24 +0900

    .

--- diffusionPractice/utils/functions.py
+++ diffusionPractice/utils/functions.py
@@ -29,27 +29,55 @@ def default(val, d):
 
 def cast_tuple(t, length = 1):
     '''
-    
+    check the 't' is the type of 'tuple'.
+    if the 't' wasn't tuple, return the tuple with t for length.
+
+    for example, t is 'a' and lenght is 3, then return ('a', 'a', 'a')
     '''
     if isinstance(t, tuple):
         return t
     return ((t,) * length)
 
 def divisible_by(numer, denom):
+    '''
+    return (numer % denom) == 0
+    '''
     return (numer % denom) == 0
 
 def identity(t, *args, **kwargs):
+    '''
+    return t
+    '''
     return t
 
 def cycle(dl):
+    '''
+    Inputs:
+        dl (list): [data1, data2, data3, ...]
+    '''
     while True:
         for data in dl:
             yield data
 
 def has_int_squareroot(num):
+    '''
+    return (math.sqrt(num) ** 2) == num
+    '''
     return (math.sqrt(num) ** 2) == num
 
 def num_to_groups(num, divisor):
+    '''
+    Example:
+        Input: num = 10, divisor = 3.
+        Ouputs: [3, 3, 3, 1].
+
+    Inputs:
+        num (int): total nums.
+        divisor (int): each num for group.
+    
+    Outputs:
+        arr (arr): devided arr.
+    '''
     groups = num // divisor
     remainder = num % divisor
     arr = [divisor] * groups
@@ -58,6 +86,10 @@ def num_to_groups(num, divisor):
     return arr
 
 def convert_image_to_fn(img_type, image):
+    '''
+    if image type is same with img_type, return image.
+    if image type is different with img_type, convert image type with 'img_type'.
+    '''
     if image.mode != img_type:
         return image.convert(img_type)
     return image
