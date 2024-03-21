07d6e9ff349c6e702617e0684cd23891c27ebd13
tjwjddn980117 <tjwjddn15584@naver.com>
2024-03-21 09:02:00 +0900

    .

--- diffusionPractice/requirements.bat
+++
@@ -1,6 +0,0 @@
-conda install conda-forge::packaging
-conda install conda-forge::einops
-conda install conda-forge::tqdm
-pip install ema-pytorch
-conda install conda-forge::accelerate
-pip install pytorch-fid
