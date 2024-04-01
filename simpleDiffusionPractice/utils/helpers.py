28b604abef8b4b3ce8222081f7ff1b911eff99be
tjwjddn980117 <tjwjddn15584@naver.com>
2024-04-01 09:07:38 +0900

    .

--- simpleDiffusionPractice/utils/helpers.py
+++ simpleDiffusionPractice/utils/helpers.py
@@ -1,14 +1,15 @@
 from torch import nn
+import math
 import torch.nn.functional as F
 import torchvision
 
 # helpers
 def exists(val):
     '''
-    the function that check the parameter is exist.
+    the function that check the parameter is exist. 
 
     Inputs:
-        val ( ): input.
+        val ( ): input. 
     
     Outputs:
         Outputs:
@@ -19,13 +20,13 @@ def exists(val):
 
 def identity(t):
     '''
-    Reinfore the parammeter.
+    Reinfore the parammeter. 
     
     Inputs:
-        t ( ): input.
+        t ( ): input. 
     
     Outputs:
-        t ( ): input -> output.
+        t ( ): input -> output. 
     '''
     return t
 
@@ -39,7 +40,7 @@ def is_lambda(f):
     Outputs:
         
     '''
-    return callable(f) and f.__name__ == "<lambda>"
+    return callable(f) and f.__name__ == "<lambda>" 
 
 def default(val, d):
     '''
@@ -59,39 +60,97 @@ def default(val, d):
 
 def cast_tuple(t, l = 1):
     '''
-    check the 't' is the type of 'tuple'.
-    if the 't' wasn't tuple, return the tuple with t for length.
+    check the 't' is the type of 'tuple'. 
+    if the 't' wasn't tuple, return the tuple with t for length. 
 
-    for example, t is 'a' and lenght is 3, then return ('a', 'a', 'a')
+    for example, t is 'a' and lenght is 3, then return ('a', 'a', 'a'). 
     '''
     return ((t,) * l) if not isinstance(t, tuple) else t
 
 def append_dims(t, dims):
     '''
-    append dims with (1, )
-    example, torch.Size([2, 3]) >> append_dims(t, 2) >> torch.Size([2, 3, 1, 1])
+    append dims with (1, ). 
+    example, torch.Size([2, 3]) >> append_dims(t, 2) >> torch.Size([2, 3, 1, 1]). 
 
     Inputs:
-        t (tensor): Some tensor.
-        dims (int): appending dims.
+        t (tensor): Some tensor. 
+        dims (int): appending dims. 
     
     Outputs:
-        _ (tensor): tensor with appending dims.
+        _ (tensor): tensor with appending dims. 
     '''
     shape = t.shape
     return t.reshape(*shape, *((1,) * dims))
 
 def l2norm(t):
     '''
-    normalized last layer.
+    normalized last layer. 
 
     Inputs:
-        t (tensor) input tensor.
+        t (tensor) input tensor. 
     
     Outputs:
         return F.normalize(t, dim = -1).
     '''
     return F.normalize(t, dim = -1)
+
+def num_to_groups(num, divisor):
+    '''
+    Example:
+        Input: num = 10, divisor = 3.
+        Ouputs: [3, 3, 3, 1].
+
+    Inputs:
+        num (int): total nums.
+        divisor (int): each num for group.
+    
+    Outputs:
+        arr (arr): devided arr.
+    '''
+    groups = num // divisor
+    remainder = num % divisor
+    arr = [divisor] * groups
+    if remainder > 0:
+        arr.append(remainder)
+    return arr
+
+def cycle(dl):
+    '''
+    Inputs:
+        dl (list): [data1, data2, data3, ...]
+    
+    Returns:
+        data1, data2,... you have to next(cycle(dl)) for work on. 
+    '''
+    while True:
+        for data in dl:
+            yield data
+
+def divisible_by(numer, denom):
+    '''
+    check the number can devide. 
+
+    Inputs:
+        numer (int): big number. 
+        denom (int): devide number. 
+    
+    Returns:
+        (numer % denom) == 0. 
+    '''
+    return (numer % denom) == 0
+
+def has_int_squareroot(num):
+    '''
+    check the 'number' can 'root'. 
+
+    Inputs:
+        num (int): check it could be. 
+    
+    Returns:
+        _ (bool): check it could be. 
+    '''
+    root = math.sqrt(num)
+    return root.is_integer()
 
 def init_img_transform(x):
     '''
