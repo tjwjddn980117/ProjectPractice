560470caa4d6ccf16754b24cb0e595ebeb927b22
tjwjddn980117 <tjwjddn15584@naver.com>
2024-04-29 09:00:29 +0900

    .

---
+++ dalle1Practice/utils.py
@@ -0,0 +1,19 @@
+import attr
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+logit_laplace_eps: float = 0.1
+
+@attr.s(eq=False)
+class Conv2d(nn.Module):
+	n_in:  int = attr.ib(validator=lambda i, a, x: x >= 1)
+	n_out: int = attr.ib(validator=lambda i, a, x: x >= 1)
+	kw:    int = attr.ib(validator=lambda i, a, x: x >= 1 and x % 2 == 1)
+
+	use_float16:   bool         = attr.ib(default=True)
+	device:        torch.device = attr.ib(default=torch.device('cpu'))
+	requires_grad: bool         = attr.ib(default=False)
+	
