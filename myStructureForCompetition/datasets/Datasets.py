db511c0ffde3d5606a1069b30d5b24789b1f3e86
tjwjddn980117 <tjwjddn15584@naver.com>
2024-05-11 22:32:59 +0900

    .

--- myStructureForCompetition/datasets/Datasets.py
+++ myStructureForCompetition/datasets/Datasets.py
@@ -1,58 +1,55 @@
-import cv2
-import random
-import numpy as np
-
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-
-import torch
+from torchvision.io import read_image
 from torch.utils.data import Dataset
 
 class CustomDataset(Dataset):
-    def __init__(self, img_path_list, label_list, transforms=None , flag_mixup = False):
-        self.img_path_list = img_path_list
-        self.label_list = label_list
-        self.transforms = transforms
-        self.flag_mixup = flag_mixup
-        
-    def __getitem__(self, index):
-        img_path = self.img_path_list[index]
-        image = cv2.imread(img_path)
-        if self.label_list is not None:
-            label = torch.zeros(len(le.classes_))
-            label[self.label_list[index]] = 1.
-            
-        # 기존 image data
-        if self.transforms is not None:
-            image = self.transforms(image=image)['image']
-        
-        if self.flag_mixup :
-            # mixup에 사용될 data 선택
-            mixup_label = torch.zeros(len(le.classes_))
+    '''
+    Default Dataset for Competition for Image dataset. 
+    '''
+    def __init__(self, df, path_col,  mode='train'):
+        '''
+        Init for default Image Dataset. 
 
-            while True:
-              mixup_idx = random.randint(0, len(self.img_path_list)-1) # 전체 데이터 중 아무거나 선택 / 중복되는 클래스가 선택될 수 있음
-              if self.label_list[mixup_idx] != self.label_list[index]: # 같은 카테고리 방지
-                mixup_label[self.label_list[mixup_idx]] = 1.
-                break
-        
-            # mix할 이미지
-            mixup_image = cv2.imread(self.img_path_list[mixup_idx])
-            if self.transforms is not None:
-                mixup_image = self.transforms(image = mixup_image)["image"]
+        Arguments:
+            df (pd.csv): dataframe of csv. 
+            path_col (str): the name of column about the information about data paths. 
+            mode (str): the mode of dataset. 
 
-            # Select a random number from the given beta distribution
-            # Mixup the images accordingly
-            alpha = 0.4
-            lam = np.random.beta(alpha, alpha)
-            image = lam * image + (1 - lam) * mixup_image
-            label = lam * label + (1 - lam) * mixup_label
-
-        # label one-hot으로 생성
-        if self.label_list is not None:
-            return image, label
-        else:
-            return image
+        Inputs:
         
+        '''
+        self.df = df
+        self.path_col = path_col
+        self.mode = mode
+
     def __len__(self):
-        return len(self.img_path_list)
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if self.mode == 'train':
+            row = self.df.iloc[idx]
+            image = read_image(row[self.path_col])/256.
+            label = row['class']
+            data = {
+                'image':image,
+                'label':label
+            }
+            return data
+        elif self.mode == 'val':
+            row = self.df.iloc[idx]
+            image = read_image(row[self.path_col])/256.
+            label = row['class']
+            data = {
+                'image':image,
+                'label':label
+            }
+            return data
+        elif self.mode == 'inference':
+            row = self.df.iloc[idx]
+            image = read_image(row[self.path_col])/256.
+            data = {
+                'image':image,
+            }
+            return data
+
+    def train_transform(self, image):
+        pass
