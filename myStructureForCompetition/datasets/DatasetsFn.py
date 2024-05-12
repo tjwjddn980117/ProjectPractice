3d5d618d4e857b7beebfa9ba41c8b0fc5081144d
tjwjddn980117 <tjwjddn15584@naver.com>
2024-05-12 09:50:38 +0900

    .

--- myStructureForCompetition/datasets/DatasetsFn.py
+++ myStructureForCompetition/datasets/DatasetsFn.py
@@ -1,4 +1,6 @@
 import torch
+from torchvision.transforms import v2 as  transforms
+from torchvision.transforms import RandomAffine, RandomHorizontalFlip, RandomVerticalFlip, ColorJitter
 
 class CustomCollateFn:
     def __init__(self, transform, mode):
@@ -25,3 +27,20 @@ class CustomCollateFn:
             return {
                 'pixel_values':pixel_values,
             }
+        
+train_transform = transforms.Compose([
+    transforms.Resize(size=(196,196), interpolation=transforms.InterpolationMode.BICUBIC),
+    RandomAffine(degrees=30, translate=(0.2, 0.2), scale=(0.8, 1.2)),
+    RandomHorizontalFlip(p=0.5),
+    ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    transforms.RandomGrayscale(p=0.1),
+    transforms.Normalize(mean=(0.485,0.456,0.406), std=(0.229,0.224,0.225)),
+])
+
+val_transform = transforms.Compose([
+    transforms.Resize(size=(196,196), interpolation=transforms.InterpolationMode.BICUBIC),
+    transforms.Normalize(mean=(0.485,0.456,0.406), std=(0.229,0.224,0.225)),
+])
+
+train_collate_fn = CustomCollateFn(train_transform, 'train')
+val_collate_fn = CustomCollateFn(val_transform, 'val')
