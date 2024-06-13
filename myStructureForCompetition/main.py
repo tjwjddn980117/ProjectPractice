83f9839497f91ed64e9eb18fd11fc7cf33ae6f71
tjwjddn980117 <tjwjddn15584@naver.com>
2024-06-13 10:33:11 +0900

    .

--- myStructureForCompetition/main.py
+++ myStructureForCompetition/main.py
@@ -5,9 +5,12 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from utils.config import CFG
 from utils.init_sharing_resources import init_label_encoder, init_wandb
 from trainers.pytorch_trainer import trainer
+import torch
 
 cvs_file_path = CFG.PROJECT_PATH + 'data\\train.csv'
 init_label_encoder(cvs_file_path, CFG.LABEL_ENCODER_NAME)
 init_wandb(CFG.WANDB_ID_NAME)
 
-trainer()
+torch.autograd.set_detect_anomaly(True)
+
+trainer(load_file='C:\\Users\\Seo\\Desktop\\Gits\\ProjectPractice\\myStructureForCompetition\\checkpoints\\fold0_epoch7.pt')
