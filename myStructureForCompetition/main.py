3ce7bf0852bb60e68a31c8352310043961293cc7
tjwjddn980117 <tjwjddn15584@naver.com>
2024-06-14 12:45:11 +0900

    .

--- myStructureForCompetition/main.py
+++ myStructureForCompetition/main.py
@@ -7,10 +7,14 @@ from utils.init_sharing_resources import init_label_encoder, init_wandb
 from trainers.pytorch_trainer import trainer
 import torch
 
+import warnings
+
+warnings.filterwarnings("ignore", message="Torch was not compiled with flash attention")
+
 cvs_file_path = CFG.PROJECT_PATH + 'data\\train.csv'
 init_label_encoder(cvs_file_path, CFG.LABEL_ENCODER_NAME)
 init_wandb(CFG.WANDB_ID_NAME)
 
 torch.autograd.set_detect_anomaly(True)
 
-trainer(load_file='C:\\Users\\Seo\\Desktop\\Gits\\ProjectPractice\\myStructureForCompetition\\checkpoints\\fold0_epoch7.pt')
+trainer()
