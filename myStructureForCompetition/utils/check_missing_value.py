5f3a2dd290f75ffea0d6d30cd6e118ce0a0bf93d
tjwjddn980117 <tjwjddn15584@naver.com>
2024-06-17 14:16:32 +0900

    .

--- myStructureForCompetition/utils/check_missing_value.py
+++ myStructureForCompetition/utils/check_missing_value.py
@@ -13,10 +13,15 @@ def zero_filtering(x: torch.Tensor) -> torch.Tensor:
     Cosine Similarity will be returned NaN, when input value has zero, like as torch.clamp()
     """
     eps = 1e-4
-    if (x <= eps).any():
-        print(f"Notice: Some values are less than or equal to eps ({eps}). Replacing with eps.")
-        x[x <= eps] = eps
-    return x
+    # x의 복사본을 생성하여 inplace 연산을 피함
+    x_filtered = x.clone()
+    
+    # 조건을 만족하는 경우에만 값을 대체하고 메시지 출력
+    if (x_filtered <= eps).any():
+        print(f"\n!!!! Notice: Some values are less than or equal to eps ({eps}). Replacing with eps.")
+        x_filtered[x_filtered <= eps] = eps
+    
+    return x_filtered
 
 def nan_filtering(x: torch.Tensor, eps: float = 1e-4) -> torch.Tensor:
     """
