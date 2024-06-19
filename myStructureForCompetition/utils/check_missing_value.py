129161adb97038aad96e6f0b1831fec0d0dceabd
tjwjddn980117 <tjwjddn15584@naver.com>
2024-06-19 09:56:39 +0900

    .

--- myStructureForCompetition/utils/check_missing_value.py
+++ myStructureForCompetition/utils/check_missing_value.py
@@ -4,6 +4,7 @@ def check_nan(x: torch.Tensor) -> bool:
     """ Check if there is NaN in tensor """
     checker = False
     if True in torch.isnan(x):
+        print(f"\n!!!! Notice: Some values about Nan")
         checker = True
     return checker
 
@@ -18,7 +19,7 @@ def zero_filtering(x: torch.Tensor) -> torch.Tensor:
     
     # 조건을 만족하는 경우에만 값을 대체하고 메시지 출력
     if (x_filtered <= eps).any():
-        print(f"\n!!!! Notice: Some values are less than or equal to eps ({eps}). Replacing with eps.")
+        # print(f"\n!!!! Notice: Some values are less than or equal to eps ({eps}). Replacing with eps.")
         x_filtered[x_filtered <= eps] = eps
     
     return x_filtered
@@ -28,4 +29,8 @@ def nan_filtering(x: torch.Tensor, eps: float = 1e-4) -> torch.Tensor:
     Change eps value for NaN Embedding, because competition metric is cosine similarity
     Cosine Similarity will be returned NaN
     """
-    return torch.nan_to_num(x, nan=eps)
+    # x의 복사본을 생성하여 inplace 연산을 피함
+    x_filtered = x.clone()
+    
+    # NaN 값을 eps로 대체
+    return torch.nan_to_num(x_filtered, nan=eps)
