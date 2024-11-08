4c277773315c443c22a497bf1db5c73f4f2aa32c
tjwjddn980117 <tjwjddn15584@naver.com>
2024-11-08 15:03:21 +0900

    .

--- dalle1_ex_Practice/model/mingpt.py
+++ dalle1_ex_Practice/model/mingpt.py
@@ -15,3 +15,20 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 logger = logging.getLogger(__name__)
+
+class DallEGPTConfig:
+    r"""
+    Minimal DallE config holding all fields requored for
+    training gpt for both text and iamge tokens
+    """
+    def __init__(self, text_vocab_size,
+                 image_vocab_size,
+                 max_sequence_len, im_size, **kwargs):
+        self.text_vocab_size = text_vocab_size
+        self.image_vocab_size = image_vocab_size
+        # Fixing block size to maximum sequence length we have seen
+        self.block_size = max_sequence_len
+        self.im_size = im_size
+        self.num_text_tokens = max_sequence_len - im_size*im_size
+        for k,v in kwargs.items():
+            setattr(self, k, v)
