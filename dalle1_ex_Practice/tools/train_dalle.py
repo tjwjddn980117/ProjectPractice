0595bfc7493565ba29069aa713b34bd72e29f2e7
tjwjddn980117 <tjwjddn15584@naver.com>
2025-01-17 09:20:11 +0900

    .

--- dalle1_ex_Practice/tools/train_dalle.py
+++ dalle1_ex_Practice/tools/train_dalle.py
@@ -15,3 +15,32 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 plt_counts = 0
 
+def train_for_one_epoch(epoch_idx, model, loader, optimizer, config):
+    r"""
+    Method to run the training for one epoch.
+    :param epoch_idx: iteration number of current epoch
+    :param model: Dalle model
+    :param mnist_loader: Data loder
+    :param optimizer: optimzier to be used taken from config
+    :param crtierion: For computing the loss
+    :param config: configuration for the current run
+    :return:
+    """
+    losses = []
+    for data in tqdm(loader):
+        im = data['image']
+        text_tokens = data['text_tokens']
+        im = im.float().to(device)
+        text = text_tokens.long().to(device)
+        optimizer.zero_grad()
+        
+        _, loss_text, loss_image = model(im, text)
+        loss = (loss_text*1 + loss_image*config['train_params']['dalle_image_loss']) / (1+config['train_params']['dalle_image_loss'])
+        losses.append(loss.item())
+        loss.backward()
+        optimizer.step()
+    print('Finished epoch: {} | Modelling Loss : {:.4f} '.
+          format(epoch_idx + 1,
+                 np.mean(losses)))
+    return np.mean(losses)
+
