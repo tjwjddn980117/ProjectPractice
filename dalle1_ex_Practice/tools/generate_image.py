7998ed5b6c23674ce1cfd10379e8a633cb972fe8
tjwjddn980117 <tjwjddn15584@naver.com>
2025-01-21 15:10:48 +0900

    .

--- dalle1_ex_Practice/tools/generate_image.py
+++ dalle1_ex_Practice/tools/generate_image.py
@@ -11,3 +11,15 @@ from model.discrete_vae import DiscreteVAE
 from model.dalle import DallE
 from datasets.mnist_color_texture_dataset import MnistVisualLanguageDataset
 from torchvision.utils import make_grid
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def infer(args):
+    ######## Read the config file #######
+    with open(args.config_path, 'r') as file:
+        try:
+            config = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+    print(config)
+    
