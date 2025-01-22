35f086ecb0b1f7dedd855a7d53559a6775359529
tjwjddn980117 <tjwjddn15584@naver.com>
2025-01-22 10:15:50 +0900

    .

--- dalle1_ex_Practice/tools/infer_dvae.py
+++ dalle1_ex_Practice/tools/infer_dvae.py
@@ -26,16 +26,3 @@ def inference(args):
             print(exc)
     print(config)
 
-def inference(args):
-    r"""
-    Method to infer discrete vae and get
-    reconstructions
-    :param args:
-    :return:
-    """
-    with open(args.config_path, 'r') as file:
-        try:
-            config = yaml.safe_load(file)
-        except yaml.YAMLError as exc:
-            print(exc)
-    print(config)
